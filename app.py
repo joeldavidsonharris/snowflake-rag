078391a94ea4984eef9ed21faa9cb8863474e71b
Joel Harris <joeldavidsonharris@gmail.com>
2024-12-05 11:40:49 +1300

    Add Snowlake RAG chatbot

---
+++ app.py
@@ -0,0 +1,115 @@
+# Imports
+from snowflake.snowpark.context import get_active_session
+from snowflake.core import Root
+import streamlit as st
+import pandas as pd
+import json
+
+pd.set_option("max_colwidth", None)
+
+# Search constants
+CORTEX_SEARCH_DATABASE = "RAG"
+CORTEX_SEARCH_SCHEMA = "POC"
+CORTEX_SEARCH_SERVICE = "VECTOR_STORE"
+NUM_CHUNKS = 10  # Number of chunks to include in RAG context
+COLUMNS = ["chunk", "relative_path"]
+CHAT_MODEL = "llama3-8b"
+
+# Create clients
+session = get_active_session()
+root = Root(session)
+svc = (
+    root.databases[CORTEX_SEARCH_DATABASE]
+    .schemas[CORTEX_SEARCH_SCHEMA]
+    .cortex_search_services[CORTEX_SEARCH_SERVICE]
+)
+
+
+def config_options():
+    st.sidebar.write(
+        "Documents to be used:"
+    )
+    docs_available = session.sql("ls @docs").collect()
+    list_docs = []
+    for doc in docs_available:
+        list_docs.append(doc["name"].split("/")[-1])
+    st.sidebar.dataframe(list_docs, column_config={"value": "Documents"})
+
+
+def get_similar_chunks_search_service(query):
+    response = svc.search(query, COLUMNS, limit=NUM_CHUNKS)
+    st.sidebar.json(response.json())
+    return response.json()
+
+
+def create_prompt(question):
+    prompt_context = get_similar_chunks_search_service(question)
+
+    prompt = f"""
+        You are an expert chat assistance that extracts information from the CONTEXT provided
+        between <context> and </context> tags.
+        When answering the question contained between <question> and </question> tags
+        be concise and do not hallucinate.
+        Only output information that you see in your CONTEXT, do not make anything up.
+        If you don't have the information just say so.
+        Only answer the question if you can extract it from the CONTEXT provided.
+        Provide your output in markdown format.
+
+        Do not reference the CONTEXT used in your answer.
+        Don't say things like "Based on the provided context".
+
+        <context>          
+        {prompt_context}
+        </context>
+        <question>  
+        {question}
+        </question>
+        Answer: 
+    """
+
+    json_data = json.loads(prompt_context)
+
+    relative_paths = set(item["relative_path"] for item in json_data["results"])
+
+    return prompt, relative_paths
+
+
+def complete(question):
+    prompt, relative_paths = create_prompt(question)
+    sql_command = "select snowflake.cortex.complete(?, ?) as response"
+
+    df_response = session.sql(
+        sql_command, params=[CHAT_MODEL, prompt]
+    ).collect()
+
+    return df_response[0].RESPONSE.replace('$', '\$'), relative_paths
+
+
+def main():
+    st.title("Documentation Assistant")
+
+    config_options()
+
+    question = st.text_input(
+        "Ask the documentation a question",
+        placeholder="Ask your question here",
+        label_visibility="collapsed",
+    )
+
+    if question:
+        response, relative_paths = complete(question)
+        st.write(response)
+
+        if relative_paths != "None":
+            with st.sidebar.expander("Related Documents"):
+                for path in relative_paths:
+                    sql_command = f"select get_presigned_url(@docs, '{path}', 360) as url_link from directory(@docs)"
+                    df_url_link = session.sql(sql_command).to_pandas()
+                    url_link = df_url_link._get_value(0, "URL_LINK")
+
+                    display_url = f"Doc: [{path}]({url_link})"
+                    st.sidebar.markdown(display_url)
+
+
+if __name__ == "__main__":
+    main()
---
+++ rag.sql
@@ -0,0 +1,140 @@
+-- Original guide: https://quickstarts.snowflake.com/guide/ask_questions_to_your_own_documents_with_snowflake_cortex_search/#0
+
+------------------ 1. Account Setup ------------------
+
+use role accountadmin;
+
+drop warehouse compute_wh;
+drop database snowflake_sample_data;
+
+alter account set
+timezone = 'Pacific/Auckland'
+week_start = 1
+timestamp_type_mapping = timestamp_ltz
+enable_unredacted_query_syntax_error = true
+cortex_enabled_cross_region = 'ANY_REGION';
+
+use role sysadmin;
+
+create warehouse admin_wh
+warehouse_size = xsmall
+auto_suspend = 60
+initially_suspended = true;
+
+create warehouse rag_wh
+warehouse_size = xsmall
+auto_suspend = 60
+initially_suspended = true;
+
+create database admin;
+create database rag;
+
+drop schema admin.public;
+drop schema rag.public;
+
+use role securityadmin;
+
+grant imported privileges on database snowflake to role securityadmin;
+grant imported privileges on database snowflake to role sysadmin;
+
+grant usage on warehouse admin_wh to role securityadmin;
+grant all on database admin to role securityadmin;
+
+use role sysadmin;
+use warehouse rag_wh;
+use database rag;
+
+create schema rag.poc;
+
+------------------ 2. Loading Data ------------------
+
+use role sysadmin;
+use warehouse rag_wh;
+use schema rag.poc;
+
+create or replace function text_chunker(pdf_text string)
+returns table (chunk varchar)
+language python
+runtime_version = '3.11'
+handler = 'text_chunker'
+packages = ('snowflake-snowpark-python', 'langchain')
+as
+$$
+# Imports
+from snowflake.snowpark.types import StringType, StructField, StructType
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pandas as pd
+
+class text_chunker:
+    def process(self, pdf_text: str):
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=512,  # adjust this as you see fit
+            chunk_overlap=256,  # allows some overlap for context retention
+            length_function=len
+        )
+        
+        chunks = text_splitter.split_text(pdf_text)
+        df = pd.DataFrame(chunks, columns=['chunks'])
+        
+        yield from df.itertuples(index=False, name=None)
+$$;
+
+create or replace stage docs
+encryption = (type = 'snowflake_sse') 
+directory = (enable = true);
+
+-- Alter filepath to match your files
+put 'file:///home/joel/projects/snowflake-rag/sky-tv-annual-report-2024.pdf' @docs auto_compress=false overwrite=true;
+
+ls @docs;
+
+------------------ 3. Data Preperation ------------------
+
+create or replace table docs_chunks ( 
+    relative_path varchar, -- relative path to the pdf file
+    size number, -- size of the pdf
+    file_url varchar, -- url for the pdf
+    scoped_file_url varchar, -- scoped url (choose which one to keep based on use case)
+    chunk varchar, -- piece of text
+);
+
+insert overwrite into docs_chunks (relative_path, size, file_url, scoped_file_url, chunk)
+select 
+    relative_path,
+    size,
+    file_url,
+    build_scoped_file_url(@docs, relative_path) as scoped_file_url,
+    func.chunk as chunk
+from directory(@docs),
+    table(text_chunker(to_varchar(snowflake.cortex.parse_document(@docs, relative_path, {'mode': 'LAYOUT'})))) as func;
+
+------------------ 4. Search Service ------------------
+
+create or replace cortex search service vector_store
+on chunk
+warehouse = rag_wh
+target_lag = '1 minute'
+as (
+    select 
+        chunk,
+        relative_path,
+        file_url
+    from docs_chunks
+);
+
+------------------ 5. Streamlit App ------------------
+
+create or replace stage code
+encryption = (type = 'snowflake_sse')
+directory = (enable = true);
+
+-- Alter filepath to match your files
+put 'file:///home/joel/projects/snowflake-rag/app.py' @code auto_compress=false overwrite=true;
+put 'file:///home/joel/projects/snowflake-rag/environment.yml' @code auto_compress=false overwrite=true;
+
+ls @code;
+
+create or replace streamlit chatbot
+root_location = '@rag.poc.code'
+main_file = 'app.py'
+query_warehouse = rag_wh;
